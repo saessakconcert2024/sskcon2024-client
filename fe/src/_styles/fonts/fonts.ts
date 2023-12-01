640125d4f869a032a5e41fac80ee2933082d2978
brgndy <brgndy96@gmail.com>
2023-12-01 10:09:55 +0900

    style(font): font 파일 export

---
+++ fe/src/_styles/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local'
+
+const suitExtraBold = localFont({ src: '/SUIT-ExtraBold.ttf' })
+const suitBold = localFont({ src: './SUIT-Bold.ttf' })
+const suitMedium = localFont({ src: '/SUIT-Medium.ttf' })
+const suitRegular = localFont({ src: '/SUIT-Regular.ttf' })
+
+export { suitExtraBold, suitBold, suitMedium, suitRegular }
