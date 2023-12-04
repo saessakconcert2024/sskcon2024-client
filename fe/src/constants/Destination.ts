e60e32446f2b2c0a5928109df9ef8f95828292b4
SWARVY <swarvy0826@naver.com>
2023-12-04 23:51:02 +0900

    feat : 지도 컴포넌트 사용을 위한 상수파일 작성

---
+++ fe/src/constants/Destination.ts
@@ -0,0 +1,9 @@
+const DESTINATION = {
+  latitude: 37.505719,
+  longtitude: 127.050677,
+  name: '우아한테크코스 선릉캠퍼스',
+  detail: '서울 강남구 테헤란로 411, 성담빌딩 13층',
+  room: '우아한형제들 테크살롱',
+}
+
+export default DESTINATION
