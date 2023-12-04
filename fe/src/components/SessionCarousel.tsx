df8002d1e5fe3fe606a9a3999550b96dcf6552c4
SWARVY <swarvy0826@naver.com>
2023-12-04 23:49:50 +0900

    feat : Carousel 추가 및 스타일링 코드 작성

---
+++ fe/src/components/SessionCarousel.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { SESSION_COMMENTS } from '@/constants/Session'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import SessionSlide from './SessionSlide'
+import style from '../style/Carousel.module.css'
+import '../style/ReactResponsiveCarousel.css'
+
+export default function SessionCarousel() {
+  return (
+    <div className={style.carouselContainer}>
+      <Carousel
+        emulateTouch
+        swipeable
+        dynamicHeight
+        showStatus={false}
+        showArrows={false}
+      >
+        {SESSION_COMMENTS.map(({ title, comments }, idx) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              rowGap: '20px',
+              alignItems: 'center',
+            }}
+          >
+            <h1>Session{idx + 1}</h1>
+            <SessionSlide key={idx} title={title} comments={comments} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  )
+}
