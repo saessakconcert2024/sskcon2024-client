4e1bb0f4ca5b9142884b806870d6b14ef553c0e3
SWARVY <swarvy0826@naver.com>
2023-12-04 23:49:12 +0900

    feat : 세션 슬라이드 컴포넌트 생성 및 스타일링

---
+++ fe/src/components/SessionSlide.tsx
@@ -0,0 +1,42 @@
+import { SessionCommentType } from '@/constants/Session'
+import style from '../style/SessionSlide.module.css'
+
+interface SessionProps {
+  title: string
+  comments: SessionCommentType[]
+}
+
+export default function SessionSlide({ title, comments }: SessionProps) {
+  return (
+    <div className={style.sessionSlideContainer}>
+      <div className={style.sessionSlideHeader}>
+        <h2>{title}</h2>
+      </div>
+      <div className={style.sessionSlideContent}>
+        {comments.map((comment, idx) =>
+          idx % 2 === 0 ? (
+            <div className={style.evenComment} key={idx}>
+              <div className={style.commentProfile}>
+                {comment.user.profileImg}
+              </div>
+              <div className={style.commentContent}>
+                <span>{comment.user.name}</span>
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ) : (
+            <div className={style.oddComment} key={idx}>
+              <div className={style.commentContent}>
+                <span>{comment.user.name}</span>
+                <p>{comment.comment}</p>
+              </div>
+              <div className={style.commentProfile}>
+                {comment.user.profileImg}
+              </div>
+            </div>
+          ),
+        )}
+      </div>
+    </div>
+  )
+}
