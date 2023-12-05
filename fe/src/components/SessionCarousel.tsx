7863ecc580cbd178845d497b7cfe487ab62063e3
SWARVY <swarvy0826@naver.com>
2023-12-05 17:49:02 +0900

    refactor : 린트 룰에 따라 import문 위치 수정

--- fe/src/components/SessionCarousel.tsx
+++ fe/src/components/SessionCarousel.tsx
@@ -3,10 +3,10 @@
 import { SESSION_COMMENTS } from '@/constants/Session'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { suitExtraBold } from '@/_styles/fonts/fonts'
 import SessionSlide from './SessionSlide'
 import style from '../style/Carousel.module.css'
 import '../style/ReactResponsiveCarousel.css'
-import { suitExtraBold } from '@/_styles/fonts/fonts'
 
 export default function SessionCarousel() {
   return (
@@ -28,7 +28,7 @@ export default function SessionCarousel() {
             }}
           >
             <h1 className={suitExtraBold.className}>Session{idx + 1}</h1>
-            <SessionSlide key={idx} title={title} comments={comments} />
+            <SessionSlide title={title} comments={comments} />
           </div>
         ))}
       </Carousel>
