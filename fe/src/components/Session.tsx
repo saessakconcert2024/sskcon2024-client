57d0a73c35030e3f6ec5ceb3d6ca10c35ce15228
SWARVY <swarvy0826@naver.com>
2023-12-06 16:56:06 +0900

    feat, refactor : session 컴포넌트 애니메이션 추가 및 rem단위 변경

--- fe/src/components/Session.tsx
+++ fe/src/components/Session.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import SessionCarousel from './SessionCarousel'
 import style from '../style/Session.module.css'
+import useScrollAnimation from '@/taeheon/_hooks/useScrollAnimation'
 
 export default function Session() {
+  const { targetRef, animation } = useScrollAnimation()
+
   return (
-    <div className={style.sessionContainer}>
+    <div
+      ref={targetRef}
+      className={`${style.sessionContainer} ${
+        animation ? style.animation : ''
+      }`}
+    >
       <SessionCarousel />
       <div className={style.applicationButton}>연사자 신청하기</div>
     </div>
